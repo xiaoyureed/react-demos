8837f4c2e6c73cca5f5566395a016eed0fe71190
xiaoyureed <775000738@qq.com>
2020-09-11 02:27:20 +0000

    Deploying to gh-pages from master 2f5ab34f20bd90ca994ad20bbd83592a73c3e1b2

---
+++ precache-manifest.38982e99535ef9fbb95bca60448488f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad787c1706ad626d4cea89fa393a1e36",
+    "url": "/react-demos/index.html"
+  },
+  {
+    "revision": "d51824ebaf391e589c32",
+    "url": "/react-demos/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "3d53e6fc308dc7c951b8",
+    "url": "/react-demos/static/css/main.5a7f72e8.chunk.css"
+  },
+  {
+    "revision": "d51824ebaf391e589c32",
+    "url": "/react-demos/static/js/2.e63d05c6.chunk.js"
+  },
+  {
+    "revision": "3d53e6fc308dc7c951b8",
+    "url": "/react-demos/static/js/main.7de4501b.chunk.js"
+  },
+  {
+    "revision": "ba331b6c01b21d166a6a",
+    "url": "/react-demos/static/js/runtime-main.1728e108.js"
+  },
+  {
+    "revision": "c6498520a74877786da5ca2375cf753d",
+    "url": "/react-demos/static/media/cow.c6498520.png"
+  },
+  {
+    "revision": "fd0a5a14c9367ebc4390a4693aa634fa",
+    "url": "/react-demos/static/media/tiger.fd0a5a14.png"
+  }
+]);
